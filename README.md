c09139a6f9ed7ae8cb1824192fbec94e88c1bbc5
VaughnGit <alvin.vaughn@outlook.com>
2023-01-11 23:04:14 +0000

    chore: added workshop content

---
+++ README.md
@@ -0,0 +1,3 @@
+# References:
+
+https://catalog.us-east-1.prod.workshops.aws/workshops/67662c95-2007-4281-ae51-5313cd7caa67/en-US/lab1/2-deploy-with-cdk
---
+++ test/workshop.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const Workshop = require('../lib/workshop-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/workshop-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new Workshop.WorkshopStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
